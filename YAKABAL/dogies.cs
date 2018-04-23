b4ef01d415e1dffeb9677eedea711feb0d9df4c3
Simpatie <mdedada@mail.ru>
2018-04-23 18:18:13 +0300

    Zeus missing

---
+++ YAKABAL/dogies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAKABAL
+{
+    class dogies
+    {
+        public string Name {get; set;}
+        public string Poroda { get; set; }
+        public float Vozrast { get; set; }
+    }
+}
